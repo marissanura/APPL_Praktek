97d2081c44d69c36fcde47f1fae95a5c67215c23
marissanura <icamarissa24@gmail.com>
2021-03-15 15:12:01 +0700

    Fill out the reporting document
    didn't change the coding

--- Week1/polymorphic_sorting/Numbers.java
+++ Week1/polymorphic_sorting/Numbers.java
@@ -1,26 +1,34 @@
-import java.util.Scanner;
+// ******************************************************  
+// Numbers.java  
+//  
+// Demonstrates selectionSort on an array of integers.  
+// ******************************************************  
 
-public class Numbers {
+import java.util.Scanner;  
+public class Numbers  
+{  
+ // --------------------------------------------  
+ // Reads in an array of integers, sorts them,  
+ // then prints them in sorted order.  
+ // --------------------------------------------  
+    public static void main (String[] args)  
+    {  
+        Integer[] intList;  
+        int size;  
 
-    // Reads in an array of integers, sorts them,
-    // then prints them in sorted order.
-    public static void main(String[] args) {
-        Integer[] intList;
-        int size;
+        Scanner scan = new Scanner(System.in);  
+        System.out.print ("\nHow many integers do you want to sort? ");   
+        size = scan.nextInt();  
+        intList = new Integer[size];
 
-        Scanner scan = new Scanner(System.in);
-        System.out.print("\nHow many integers do you want to sort? ");
-        size = scan.nextInt();
-        intList = new Integer[size];    
-
-        System.out.println("\nEnter the numbers...");
-        for (int i = 0; i < size; i++)
+        System.out.println ("\nEnter the numbers...");   
+        for (int i = 0; i < size; i++)  
             intList[i] = scan.nextInt();
         scan.close();
-        // Sorting.selectionSort(intList);
-        Sorting.insertionSort(intList);
-        System.out.println("\nYour numbers in sorted order...");
-        for (int i = 0; i < size; i++)
-            System.out.print(intList[i] + " ");
-    }
-}
+          
+        Sorting.selectionSort(intList); 
+        System.out.println ("\nYour numbers in sorted order...");   for (int i = 0; i < size; i++)  
+        System.out.print(intList[i] + " ");  
+        System.out.println ();  
+    }  
+} 
