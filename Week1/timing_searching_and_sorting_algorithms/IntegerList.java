8ba8ca6bf6cd19fbf825f7213981716a5253b93d
marissanura <icamarissa24@gmail.com>
2021-03-16 21:29:38 +0700

    copy pasting from previous problem (integerList.java)

--- Week1/timing_searching_and_sorting_algorithms/IntegerList.java
+++ Week1/timing_searching_and_sorting_algorithms/IntegerList.java
@@ -67,12 +67,68 @@ public class IntegerList
     //------------------------------------------------------------ 
     // sortIncreasing -- uses selection sort 
     //------------------------------------------------------------ 
-    public void sortIncreasing()
-    {
-        for (int i=0; i<list.length-1; i++)
-        {
-            int minIndex = minIndex(list, i);
-            swap(list, i, minIndex);
+    public void sortIncreasing() {
+        int minIndex;
+        for (int i = 0; i < list.length - 1; i++) {
+            // find smallest element in list starting at location i
+            minIndex = i;
+            for (int j = i + 1; j < list.length; j++)
+                if (list[j] < list[minIndex])
+                    minIndex = j;
+            // swap list[i] with smallest element
+            int temp = list[i];
+            list[i] = list[minIndex];
+            list[minIndex] = temp;
         }
     }
+
+    //------------------------------------------------------------ 
+    // sortDecreasing -- uses selection sort 
+    //------------------------------------------------------------ 
+    public void sortDecreasing()
+    {
+        int maxIndex;
+        for (int i = 0; i < list.length - 1; i++) {
+            // find biggest element in list starting at location i
+            maxIndex = i;
+            for (int j = i + 1; j < list.length; j++)
+                if (list[j] > list[maxIndex])
+                    maxIndex = j;
+            // swap list[i] with smallest element
+            int temp = list[i];
+            list[i] = list[maxIndex];
+            list[maxIndex] = temp;
+        }  
+    }
+    public int binarySearchD(int target)
+    {
+        return binarySearchDRec(target,0,list.length - 1);
+    }
+
+    private int binarySearchDRec(int target, int left, int right)
+    {
+        // target isn't in the list
+        if (left > right) 
+        {
+            return -1;
+        }
+
+        int mid = (left + right) / 2;
+        if (target == list[mid]) 
+        {
+            return mid;
+        } 
+        // if the target is bigger than the middle position,
+        // then it should search in left side
+        else if (target > list[mid])
+        {
+            return binarySearchDRec(target, left, mid - 1);
+        }
+        // if the target is smaller than the middle position,
+        // then it should search in right side
+        else
+        {
+            return binarySearchDRec(target, mid + 1, right);
+        }
+    }  
 }
